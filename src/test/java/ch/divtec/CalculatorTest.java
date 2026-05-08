891dc464464daf5520d8fbdd846cde889ee05c21
Leny Mory <leny.mory@divtec.ch>
2026-05-08 13:36:23 +0200

    Tests les fonctions de calculatrice

--- src/test/java/ch/divtec/CalculatorTest.java
+++ src/test/java/ch/divtec/CalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTests {
+public class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
 
